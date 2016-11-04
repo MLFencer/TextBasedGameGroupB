c24c91812019092bf2e6564196a033438a8dec4e
Tyler Couzens <ty.couzens@gmail.com>
2016-11-04 09:20:26 -0400

    Create Item.java

---
+++ src/Model/Item.java
@@ -0,0 +1,15 @@
+public class Item 
+{
+    private String name;
+    private double weight;
+ 
+    public Item(String n, double a) {
+        this.name = n;
+        this.attack_power = a;
+    }
+}
+
+Item[] items = {
+                 {new Item("Sword", 10), new Item("Knife", 7}, new Item("Gun", 30},
+                 {new Item("Excalibur", 100)
+               };
