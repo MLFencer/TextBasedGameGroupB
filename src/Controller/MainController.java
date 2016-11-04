a5e910d75dfada4b558a37586b3e223bde8b6a47
Michael Lott <MLFencer@users.noreply.github.com>
2016-11-04 00:37:19 -0400

    merged Main switch and attack switch with everything else.

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -9,8 +9,10 @@ import javafx.scene.control.TextField;
 public class MainController {
 	@FXML private Button btnNorth, btnSouth, btnEast, btnWest;
 	@FXML private TextArea txtAreaRoom, txtAreaEvents, txtAreaInventory, txtAreaActions, txtAreaLog,
-					  txtAreaLoot, txtAreaEnemies;
+	txtAreaLoot, txtAreaEnemies;
 	@FXML private TextField txtPlayerActions;
+
+	public String gameStatus;
 
 	// Method for assigning cardinality buttons to input into txtPlayerActions.
 	public void clickOnCompass(ActionEvent event)
@@ -24,6 +26,70 @@ public class MainController {
 		if(event.getSource() == btnWest)
 			txtPlayerActions.appendText("West");
 	}
+
+	/*
+	 * Marcel, Call the 'mainRunner()' method into the the listener for the enter so that it is called everytime
+	 * 			the enter key is hit, everything else will work itself out.
+	 *
+	 * Thanks, Michael
+	 */
+
+	public void mainRunner(){
+		if(!gameStatus.equals("fighting")){
+			String command = txtPlayerActions.getText();
+			switch (command){
+			case "north":
+				goNorth();
+				break;
+			case "south":
+				goSouth();
+				break;
+			case "west":
+				goWest();
+				break;
+			case "east":
+				goEast();
+				break;
+			case "attack":
+				gameStatus = "fighting";
+				//something along the lines of
+				// "level.getmonster().takedamage();
+				attack();
+				break;
+			default:
+				txtAreaLog.setText("Unknown Command!");
+				break;
+			}
+		} else{
+			attack();
+		}
+	}
+
+	public void attack(){
+		String command = txtPlayerActions.getText();
+		switch(command){
+		case "attack":
+			//level.getmonster().takedamage();
+			break;
+		default:
+			txtAreaLog.setText("Unknown Command!");
+			break;
+		} //more cases and more stuff coming soon.
+		/*
+		 * this will bring back the main switch instead of the atack switch.
+		 *
+		 * if (level.getMonster().getHp()<=0 || player.getHp()<=0){
+		 * 	gameStatus = "main";
+		 * }
+		 */
+	}
+
+	public void goNorth(){}
+	public void goSouth(){}
+	public void goWest(){}
+	public void goEast(){}
+
+
 
 	// Method for placing text into room description
 }
