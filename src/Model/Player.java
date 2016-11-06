3109133e116b15bb68bbd9b835d1098e54550e95
Maurice-Siegl <mauricesiegl@gmx.net>
2016-11-06 15:27:43 -0500

    Update Player.java

--- src/Model/Player.java
+++ src/Model/Player.java
@@ -1,5 +1,23 @@
 package Model;
 
-public class Player {
+public class Player extends Entity {
+	
+	int xPos, yPos;
+
+	public int getxPos() {
+		return xPos;
+	}
+
+	public void setxPos(int xPos) {
+		this.xPos = xPos;
+	}
+
+	public int getyPos() {
+		return yPos;
+	}
+
+	public void setyPos(int yPos) {
+		this.yPos = yPos;
+	}
 
 }
