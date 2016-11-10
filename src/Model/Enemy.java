3464bee2ba9516f7d922634a8f15d1eb859aa585
Maurice-Siegl <mauricesiegl@gmx.net>
2016-11-10 17:38:13 -0500

    level system

--- src/Model/Enemy.java
+++ src/Model/Enemy.java
@@ -2,7 +2,7 @@ package Model;
 
 public class Enemy extends Entity
 {
-	public Enemy(String nameIn, int hpIn, int dmgIn, int strIn, int dexIn, int conIn, int xpIn, int levelIn) {
+	public Enemy(String nameIn, int hpIn, int dmgIn, int strIn, int dexIn, int conIn, double xpIn, int levelIn) {
 		super(nameIn, hpIn, dmgIn, strIn, dexIn, conIn, xpIn, levelIn);
 		
 	}
