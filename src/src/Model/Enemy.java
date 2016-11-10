b171820c6362eb728dceb5338379e1a2f3dd9e00
Marcel Poblet <Mopobl0659@ung.edu>
2016-11-10 17:27:25 -0500

    Revert "xp system"
    This reverts commit b05684d851aacc722e89577dfd2a8c7b3204a731.

--- src/src/Model/Enemy.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-public class Enemy extends Entity
-{
-	public Enemy(String nameIn, int hpIn, int dmgIn, int strIn, int dexIn, int conIn, int xpIn, int levelIn) {
-		super(nameIn, hpIn, dmgIn, strIn, dexIn, conIn, xpIn, levelIn);
-		
-	}
-}
