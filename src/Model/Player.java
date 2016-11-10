8f3f7d7b39698b9fcfc383bc34a9e5cd7c687725
Maurice-Siegl <mauricesiegl@gmx.net>
2016-11-10 12:43:36 -0500

    updated player

--- src/Model/Player.java
+++ src/Model/Player.java
@@ -1,23 +1,49 @@
 package Model;
+import java.util.ArrayList;
 
-public class Player extends Entity {
+public class Player extends Entity 
+{
 	
-	int xPos, yPos;
-
-	public int getxPos() {
-		return xPos;
+	private int x, y;
+	private ArrayList inventory = new ArrayList<Item>();
+	
+	
+	public Player(String nameIn, int hpIn, int dmgIn, int strIn, int dexIn, int conIn, int xIn, int yIn) 
+	{
+		super(nameIn, hpIn, dmgIn, strIn, dexIn, conIn);
+		x = xIn;
+		y = yIn;
 	}
 
-	public void setxPos(int xPos) {
-		this.xPos = xPos;
+	
+	public int getX() 
+	{
+		return x;
 	}
 
-	public int getyPos() {
-		return yPos;
+	public void setX(int x) 
+	{
+		this.x = x;
 	}
 
-	public void setyPos(int yPos) {
-		this.yPos = yPos;
+	public int getY() 
+	{
+		return y;
+	}
+
+	public void setY(int y) 
+	{
+		this.y = y;
+	}
+
+	public ArrayList getInventory() 
+	{
+		return inventory;
+	}
+
+	public void setInventory(ArrayList inventory) 
+	{
+		this.inventory = inventory;
 	}
 
 }
