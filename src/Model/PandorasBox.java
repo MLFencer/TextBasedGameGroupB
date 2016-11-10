abbc0162eec9ba764abbc4bbfed5b8dc899740dd
Tyler Couzens <ty.couzens@gmail.com>
2016-11-10 17:14:59 -0500

    Update PandorasBox.java

--- src/Model/PandorasBox.java
+++ src/Model/PandorasBox.java
@@ -9,19 +9,19 @@ public class PandorasBox {
 	private Enemy e3= new Enemy("Kree Warrior",100,10,0,0,0);
 	private Enemy e4= new Enemy("Skeleton Warrior",100,10,0,0,0);
 	private Enemy e5= new Enemy("Living Armor",100,10,0,0,0);
-	private Enemy e5= new Enemy("Living Shadow",100,10,0,0,0);
-	private Enemy e5= new Enemy("Goblin",100,10,0,0,0);
-	private Enemy e5= new Enemy("Assasin",100,10,0,0,0);
+	private Enemy e6= new Enemy("Living Shadow",100,10,0,0,0);
+	private Enemy e7= new Enemy("Goblin",100,10,0,0,0);
+	private Enemy e8= new Enemy("Assasin",100,10,0,0,0);
 
 	private Item i1= new Item("Excalibur", 16, Item.types.Weapon);
 	private Item i2= new Item("Iron Sword",8, Item.types.Weapon);
 	private Item i3= new Item("War Hammer",12, Item.types.Weapon);
 	private Item i4= new Item("Cutlass", 9, Item.types.Weapon);
-	private Item i4= new Item("Club", 10, Item.types.Weapon);
-	private Item i4= new Item("Spell Tome", 14, Item.types.Weapon);
-	private Item i5= new Item("Bandage",20, Item.types.Meds);
-	private Item i6= new Item("Healing Potion",100, Item.types.Meds);
-	private Item i6= new Item("Soothing Stone",50, Item.types.Meds);
+	private Item i5= new Item("Club", 10, Item.types.Weapon);
+	private Item i6= new Item("Spell Tome", 14, Item.types.Weapon);
+	private Item i7= new Item("Bandage",20, Item.types.Meds);
+	private Item i8= new Item("Healing Potion",100, Item.types.Meds);
+	private Item i9= new Item("Soothing Stone",50, Item.types.Meds);
 
 	public ArrayList<Item> getItems(){
 		ArrayList<Item>i=new ArrayList<Item>();
@@ -31,6 +31,9 @@ public class PandorasBox {
 		i.add(i4);
 		i.add(i5);
 		i.add(i6);
+		i.add(i7);
+		i.add(i8);
+		i.add(i9);
 		return i;
 	}
 	public ArrayList<Enemy> getEnemies(){
@@ -40,6 +43,9 @@ public class PandorasBox {
 		e.add(e3);
 		e.add(e4);
 		e.add(e5);
+		e.add(e6);
+		e.add(e7);
+		e.add(e8);
 		return e;
 	}
 }
