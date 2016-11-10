956863d629a7c3e166995b7881e1b81a4cc41441
Maurice-Siegl <mauricesiegl@gmx.net>
2016-11-10 17:20:33 -0500

    updated

--- src/Model/Enemy.java
+++ src/Model/Enemy.java
@@ -2,11 +2,8 @@ package Model;
 
 public class Enemy extends Entity
 {
-
-	public Enemy(String nameIn, int hpIn, int dmgIn, int strIn, int dexIn, int conIn) 
-  {
-		super(nameIn, hpIn, dmgIn, strIn, dexIn, conIn);
+	public Enemy(String nameIn, int hpIn, int dmgIn, int strIn, int dexIn, int conIn, int xpIn, int levelIn) {
+		super(nameIn, hpIn, dmgIn, strIn, dexIn, conIn, xpIn, levelIn);
 		
 	}
-
 }
